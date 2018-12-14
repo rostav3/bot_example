7ce4e89095dae32cd3896326a1ebb7243596f0c3
stav rockah <stav.r.24@gmail.com>
2018-12-14 12:02:51 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 const DIRECTIONS = [[0,1],[0,-1],[1,1],[1,-1],[1,0],[-1,1],[-1,-1],[-1,0]];
-let socket = io.connect('http://localhost:7000');
+let socket = io.connect('http://192.168.0.254:7000');
 let user = findGetParameter("name");
 let board;
 let moves;
 
-socket.emit("connect-game", user);
 socket.on('turn-' + user, function (board) {
     moves = [];
 
     spread(board);
     socket.emit("finish-turn", {"user":user, "moves":moves});
 });
+
+socket.emit("connect-game", user);
 
 
 function spread(board) {
@@ -19,15 +20,18 @@ function spread(board) {
         let directions = geDirections(board, cells[j].x, cells[j].y);
 
         let numSoldiersToMove = Math.floor((cells[j].val - 1) / directions.length);
-        for (let i = 0; i < directions.length; i++) {
-            let direction = directions[i];
-            moves.push({
-                "num": numSoldiersToMove,
-                "prev_x": cells[j].x,
-                "prev_y": cells[j].y,
-                "next_x": cells[j].x + direction[0],
-                "next_y": cells[j].y + direction[1]
-            });
+        if (numSoldiersToMove !== 0) {
+
+            for (let i = 0; i < directions.length; i++) {
+                let direction = directions[i];
+                moves.push({
+                    "num": numSoldiersToMove,
+                    "prev_x": cells[j].x,
+                    "prev_y": cells[j].y,
+                    "next_x": cells[j].x + direction[0],
+                    "next_y": cells[j].y + direction[1]
+                });
+            }
         }
     }
 }
@@ -65,7 +69,7 @@ function geDirections(board, x, y) {
         let direction = DIRECTIONS[i];
         if ((x + direction[0] < board.length) && (x + direction[0] >= 0) &&
             (y + direction[1] < board.length) && (y + direction[1] >= 0) &&
-            (!isNaN(board[x + direction[0]][y + direction[1]])) && (board[x + direction[0]][y + direction[1]] <= 0)) {
+            (board[x + direction[0]][y + direction[1]]!==null) && (board[x + direction[0]][y + direction[1]] <= 0)) {
             directions.push(direction);
         }
     }
@@ -77,7 +81,7 @@ function canSpread(board, x, y) {
         let direction = DIRECTIONS[i];
         if ((x + direction[0] < board.length) && (x + direction[0] >= 0) &&
             (y + direction[1] < board.length) && (y + direction[1] >= 0) &&
-            (!isNaN(board[x + direction[0]][y + direction[1]])) && (board[x + direction[0]][y + direction[1]] <= 0)) {
+            (board[x + direction[0]][y + direction[1]]!==null) && (board[x + direction[0]][y + direction[1]] <= 0)) {
             return true;
         }
     }
